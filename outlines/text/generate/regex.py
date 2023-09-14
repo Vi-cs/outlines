50b092949972a41786c1a369a8005470fcc104fc
Vi-cs <58989498+Vi-cs@users.noreply.github.com>
2023-09-14 15:26:11 +0200

    Update regex.py

--- outlines/text/generate/regex.py
+++ outlines/text/generate/regex.py
@@ -244,8 +244,8 @@ class Regex(Continuation):
         mask = torch.concatenate(masks, dim=0)
 
         if Params.verbose:
-            print(f'len(logits):{len(logits)} - logits:{logits}')
-            print(f'len(mask):{len(mask)} - mask:{mask}')
+            print(f'len(logits):{len(logits[0])} - logits:{logits}')
+            print(f'len(mask):{len(mask[0])} - mask:{mask}')
 
         top_1_without_mask_values, top_1_without_mask_indices = torch.topk(logits, 1, dim=-1)
         top_1_with_mask_values, top_1_with_mask_indices = torch.topk(logits + mask, 1, dim=-1)
